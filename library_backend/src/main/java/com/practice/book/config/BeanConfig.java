79344782ac2ead1116c9fa109a1351f35e594a93
ThinzarMayMyat02 <maymyatthinzar10@gmail.com>
2025-04-20 15:53:29 +0630

    add auditing functionality

--- library_backend/src/main/java/com/practice/book/config/BeanConfig.java
+++ library_backend/src/main/java/com/practice/book/config/BeanConfig.java
@@ -3,6 +3,7 @@ package com.practice.book.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -33,5 +34,10 @@ public class BeanConfig {
     @Bean
     public AuthenticationManager authenticationConfig(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware(){
+        return new ApplicationAuditAware();
     }
 }
